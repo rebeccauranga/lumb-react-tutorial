e73e4285d27493dc7bdefca12002ad5c86d26e5d
Rebecca Uranga <urangarebecca@gmail.com>
2019-05-13 19:05:26 -0400

    renders movie information as you click on it by id

--- src/Movie.js
+++ src/Movie.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export default class Movie extends Component {
-    static propTypes = {
-        movie: PropTypes.shape({
-            title: PropTypes.string.isRequired
-        })
-    }
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154'
 
-    static defaultProps = {
+const Movie = ({ movie }) => (
+    <div>
+        <Link to={`/${movie.id}`}>
+            <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+        </Link>
+    </div>
+);               
 
-    }
+export default Movie;
 
-
-    render() {
-        return (
-            <div>
-                <h3>{this.props.movie.title}</h3>
-              </div>
-        );
-    }
+Movie.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+    }).isRequired,
 }
