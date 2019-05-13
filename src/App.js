afd56117725619ecc03d1537c2526cdbd08770e1
Rebecca Uranga <urangarebecca@gmail.com>
2019-05-13 16:40:12 -0400

    creates movie component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Movie from './Movie';
+
+const movies = [
+  {
+    id: 1, 
+    title: 'Star Wars'
+  }, {
+    id: 2, 
+    title: 'Spider Man'
+  }, {
+    id: 3, 
+    title: 'Titanic'
+  }, {
+    id: 4, 
+    title: 'Jaws'
+  }
+];
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </div>
+    );
+  }
+
+
 }
+
 
 export default App;
